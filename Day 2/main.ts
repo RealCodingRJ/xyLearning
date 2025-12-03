b12b59e76572e88d44208cbcc59701ec43efe441
RYAN JAMES <realcoder2023@gmail.com>
2025-12-03 17:49:29 -0500

    REMOVED COMMENTS

--- Day 2/main.ts
+++ Day 2/main.ts
@@ -1,8 +1,6 @@
-/* =========================== START OF STRING ================================= */
-
-let name = "RealCodingRJ";
-let message: string = "Hello " + name;
-let greeting: string = `Good Afternoon ${name}`;
+let names = "RealCodingRJ";
+let message: string = "Hello " + names;
+let greeting: string = `Good Afternoon ${names}`;
 
 console.log(message);
 console.log(greeting);
@@ -13,8 +11,6 @@ let text: string = "TypeScript is Awesome";
 console.log(text.toUpperCase());
 console.log(text.substring(0, 10));
 
-/* =========================== START OF NUMBERS ================================= */
-
 let age: number = 26;
 let price: number = Number(10) / Number(3);
 // 3.333 bc 10 * 3 = 3.333
@@ -23,7 +19,7 @@ console.log(parseInt(price.toFixed(1)));
 
 console.log(age);
 
-// Maths Calclations
+// Maths Calculations
 
 let posX = 12;
 let answer = (posX * 2) / 2;
@@ -36,8 +32,6 @@ console.log(answer === posX);
 let pi: number = 3.14159;
 console.log(pi.toFixed(2));
 console.log(pi.toString());
-
-/* ============================ START OF BOOLEANS  ================================ */
 
 let isActive: boolean = false;
 let isCompleted: boolean = true;
@@ -52,15 +46,11 @@ console.log(hasPermission && isLoggedIn);
 console.log(hasPermission || isLoggedIn);
 console.log(!isLoggedIn);
 
-/* ============================ START OF ARRAYS  ================================ */
-
 let num: number[] = [1, 2, 3, 4];
-let names: Array<string> = ["James", "McDonalds", "Applebees"];
+let namesOfPeople: Array<string> = ["James", "McDonolds", "Applebees"];
 
 console.log(num);
-console.log(names);
-
-/* ============================  ARRAY METHODS  ================================ */
+console.log(namesOfPeople);
 
 let numbers: number[] = [1, 2, 4, 5, 9];
 numbers.push(6);
@@ -69,7 +59,21 @@ console.log(numbers);
 let doubled = numbers.map((num) => num * 2);
 console.log(doubled);
 
-/* ============================  TUPLES DEFINE ================================ */
-
 let personName: [string, number] = ["Ryan", 26];
 console.log(personName);
+
+let cords: [number, number] = [10 / 2, 20 / 2];
+
+cords.push(25 / 5);
+
+cords.map((e, index) => {
+  console.log(index, e);
+});
+
+const Color = {
+  Red: "RED",
+  Green: "GREEN",
+  Blue: "BLUE",
+} as const;
+
+console.log(Color.Blue);
